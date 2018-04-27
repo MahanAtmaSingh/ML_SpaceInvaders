a38380211133e4e24d20c10e97575a01a2e7fad2
Mahan Atma Singh <mahan.atma.singh@email.de>
2018-04-27 20:47:28 +0200

    Adds some ignoring of files to by developed by VS-Express

---
+++ machine_learning_space_invaders.py
@@ -0,0 +1,23 @@
+
+import sys, getopt, os
+
+if( __name__ == '__main__'):
+    print("Invoke was:", sys.argv)
+    generalInformation = False
+    if("--version" in sys.argv or "-v" in sys.argv):
+        print("Here should be the SHA1 or the last tag...")
+        generalInformation = True
+    if("--help" in sys.argv or "-h" in sys.argv):
+        print("This software shall show some machine learning example by Space Invaders")
+        generalInformation = True
+
+    abspath = os.path.abspath(__file__)
+    dname = os.path.dirname(abspath)
+    print("The execution path:", abspath, "... and: ", dname)
+
+    newExecutionPath = dname+"/Space_Invaders"
+    os.chdir(newExecutionPath)
+    print("Changed execution path to", newExecutionPath)
+    
+    from spaceinvaders import *
+    game.main()
